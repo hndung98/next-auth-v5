c3c39bdccbd8d4a18a9b52f71c086b68ced44638
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-02 16:52:10 +0700

    update auth page

---
+++ app/auth/layout.tsx
@@ -0,0 +1,9 @@
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full flex items-center justify-center bg-radial-[at_0%_25%] from-sky-300 via-blue-500 to-indigo-900 to-90%">{children}</div>
+  );
+}
--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 type LoginButtonProps = {
   children: React.ReactNode;
   mode?: "modal" | "redirect";
@@ -11,9 +13,10 @@ export const LoginButton = ({
   mode = "redirect",
   asChild,
 }: LoginButtonProps) => {
+  const router = useRouter();
   const onClick = () => {
-    console.log("Login clicked.");
     console.log({ mode, asChild });
+    router.push("/auth/login");
   };
   if (mode === "modal") {
     return <span>To do: implement modal</span>;
