1ed5316aed7caf0c1a3c87c4202e769c7df8aad5
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-04 11:21:36 +0700

    register new user

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 import { LoginSchema, RegisterSchema } from "@/schemas";
+import { prisma } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -14,8 +17,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
+
   if (!validatedFields.success) {
     return { error: "Something went wrong!" };
   }
-  return { success: "Email sent!" };
+
+  const { email, password, name } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: { email: email, name: name, password: hashedPassword },
+  });
+
+  return { success: "User created!" };
 };
