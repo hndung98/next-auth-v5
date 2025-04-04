0e7e9663f7e80b3a248f901d2ec22fe58de770f4
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-04 16:41:20 +0700

    handling middleware and login

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,18 +1,42 @@
 "use server";
 
 import { z } from "zod";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { LoginSchema, RegisterSchema } from "@/schemas";
 import { prisma } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Something went wrong!" };
   }
-  return { success: "Email sent!" };
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials." };
+        case "CallbackRouteError":
+          return { error: "Route error." };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,20 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+export default async function Page() {
+  const session = await auth();
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>{JSON.stringify(session)}</p>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </div>
+  );
+}
