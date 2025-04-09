2539604a3c4dc63460742c29d3b15e64010554cc
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-09 09:15:29 +0700

    redirect after sign out

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
       <form
         action={async () => {
           "use server";
-          await signOut();
+          await signOut({ redirectTo: "/" });
         }}
       >
         <Button type="submit">Log out</Button>
