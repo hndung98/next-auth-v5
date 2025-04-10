a59f1dcbb06d42d058904d435d43900a2c095e82
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-10 16:13:02 +0700

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,43 @@
-import { Poppins } from "next/font/google";
+"use client";
 
-import { LoginButton } from "@/components/auth/login-button";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { Inter, Lusitana, Poppins } from "next/font/google";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import { getGreeting } from "@/lib/utils";
 
 const font = Poppins({
   subsets: ["latin"],
   weight: ["600"],
 });
+const inter = Inter({ subsets: ["latin"] });
+const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
+  const [clientGreeting, setClientGreeting] = useState("");
+
+  useEffect(() => {
+    setClientGreeting(getGreeting());
+  }, []);
   return (
-    <main className="flex h-full flex-col justify-center items-center bg-radial-[at_0%_25%] from-sky-300 via-blue-500 to-indigo-900 to-90%">
-      <div className="space-y-6 w-full text-center">
-        <h1
-          className={cn(
-            "text-6xl font-semibold text-white drop-shadow-md",
-            font.className
-          )}
-        >
-          🔐 Auth
-        </h1>
-        <p className="text-white">A simple auth service</p>
-        <div>
-          <LoginButton>
-            <Button
-              size="lg"
-              variant="secondary"
-              className="cursor-pointer"
-            >
-              Sign in
-            </Button>
-          </LoginButton>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>{clientGreeting}</strong>
+          </p>
+          <Link
+            href="/auth/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Login now</span> {"-->"}
+          </Link>
         </div>
-        <footer>___</footer>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"></div>
       </div>
     </main>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getGreeting = (): string => {
+  if (typeof window === "undefined") {
+    return "Hello! 👋"; // avoid errors on SSR
+  }
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    return "Good morning! ☀️";
+  } else if (hour >= 12 && hour < 18) {
+    return "Good afternoon! 🌞";
+  } else if (hour >= 18 && hour < 21) {
+    return "Good evening! 🌆";
+  } else {
+    return "Good night! 🌙";
+  }
+};
