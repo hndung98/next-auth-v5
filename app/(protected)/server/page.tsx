3f860133ffe1402d4951b914938394eedb5d51ef
hndung98 <54524235+hndung98@users.noreply.github.com>
2025-04-14 22:35:11 +0700

    add meta data

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
+
 import { UserInfo } from "@/components/auth/user-info";
 import { currentUser } from "@/lib/auth";
+
+export const metadata: Metadata = {
+  title: "Server",
+};
 
 export default async function Page() {
   const user = await currentUser();
