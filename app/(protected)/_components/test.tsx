413d458faddef94c8220e7c0984a177eb436eb98
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-17 13:45:45 +0700

    add pending status

--- app/(protected)/_components/test.tsx
+++ app/(protected)/_components/test.tsx
@@ -8,67 +8,94 @@ import { RoleGateInfo } from "@/components/auth/role-gate";
 import { FormSuccess } from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useTransition } from "react";
 
 export function AdminComponentExample() {
+  const [isPending, startTransition] = useTransition();
+
   const onApiRouteClick = () => {
-    fetch("/api/admin").then((res) => {
-      if (res.ok) {
-        toast.success("Success!");
-      } else if (res.status === 403) {
-        toast.error("Forbidden API Route.");
-      } else {
+    startTransition(async () => {
+      try {
+        const res = await fetch("/api/admin");
+        if (res.ok) {
+          toast.success("Success!");
+        } else if (res.status === 403) {
+          toast.error("Forbidden API Route.");
+        } else {
+          toast.error("Something went wrong...");
+        }
+      } catch (error) {
+        console.log(error);
         toast.error("Something went wrong...");
       }
     });
   };
   const onApiActionClick = () => {
-    admin().then((data) => {
-      if (data.error) {
-        toast.error(data.error);
-      }
-      if (data.success) {
-        toast.success(data.success);
+    startTransition(async () => {
+      try {
+        const data = await admin();
+        if (data.error) {
+          toast.error(data.error);
+        }
+        if (data.success) {
+          toast.success(data.success);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("Something went wrong...");
       }
     });
   };
 
   const onSeedDataClick = () => {
-    const formData = new FormData();
-    formData.append("type", "create");
-    fetch("/api/seed", {
-      method: "POST",
-      body: formData,
-    }).then((res) => {
-      if (res.ok) {
-        toast.success("Seeded!");
-      } else if (res.status === 403) {
-        toast.error("Forbidden API Route!");
-      } else if (res.status === 405) {
-        toast.error("Method Not Allowed! (must be super admin)");
-      } else if (res.status === 500) {
-        toast.error("Internal Error.");
-      } else {
-        console.log(res);
+    startTransition(async () => {
+      try {
+        const formData = new FormData();
+        formData.append("type", "create");
+        const res = await fetch("/api/seed", {
+          method: "POST",
+          body: formData,
+        });
+        if (res.ok) {
+          toast.success("Seeded!");
+        } else if (res.status === 403) {
+          toast.error("Forbidden API Route!");
+        } else if (res.status === 405) {
+          toast.error("Method Not Allowed! (must be super admin)");
+        } else if (res.status === 500) {
+          toast.error("Internal Error.");
+        } else {
+          console.log(res);
+          toast.error("Something went wrong...");
+        }
+      } catch (error) {
+        console.log(error);
         toast.error("Something went wrong...");
       }
     });
   };
   const onDeleteSeededDataClick = () => {
-    const formData = new FormData();
-    formData.append("type", "delete");
-    fetch("/api/seed", {
-      method: "POST",
-      body: formData,
-    }).then((res) => {
-      if (res.ok) {
-        toast.success("Deleted!");
-      } else if (res.status === 403) {
-        toast.error("Forbidden API Route");
-      } else if (res.status === 405) {
-        toast.error("Method Not Allowed! (must be super admin)");
-      } else if (res.status === 500) {
-        toast.error("Internal Error.");
-      } else {
+    startTransition(async () => {
+      try {
+        const formData = new FormData();
+        formData.append("type", "delete");
+        const res = await fetch("/api/seed", {
+          method: "POST",
+          body: formData,
+        });
+        if (res.ok) {
+          toast.success("Deleted!");
+        } else if (res.status === 403) {
+          toast.error("Forbidden API Route");
+        } else if (res.status === 405) {
+          toast.error("Method Not Allowed! (must be super admin)");
+        } else if (res.status === 500) {
+          toast.error("Internal Error.");
+        } else {
+          toast.error("Something went wrong...");
+        }
+      } catch (error) {
+        console.log(error);
         toast.error("Something went wrong...");
       }
     });
@@ -86,23 +113,47 @@ export function AdminComponentExample() {
         </RoleGateInfo>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 font-medium">
           <p className="text-sm font-medium">{"(Admin-only) Route X"}</p>
-          <Button onClick={onApiRouteClick}>Execute</Button>
+          <Button
+            disabled={isPending}
+            onClick={onApiRouteClick}
+            className="cursor-pointer"
+          >
+            Execute
+          </Button>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 font-medium">
           <p className="text-sm font-medium">{"(Admin-only) Action X"}</p>
-          <Button onClick={onApiActionClick}>Execute</Button>
+          <Button
+            disabled={isPending}
+            onClick={onApiActionClick}
+            className="cursor-pointer"
+          >
+            Execute
+          </Button>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 font-medium">
           <p className="text-sm font-medium">
             {"(Super-admin-only) Seed data"}
           </p>
-          <Button onClick={onSeedDataClick}>Execute</Button>
+          <Button
+            disabled={isPending}
+            onClick={onSeedDataClick}
+            className="cursor-pointer"
+          >
+            Execute
+          </Button>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 font-medium">
           <p className="text-sm font-medium">
             {"(Super-admin-only) Delete seeded data"}
           </p>
-          <Button onClick={onDeleteSeededDataClick}>Execute</Button>
+          <Button
+            disabled={isPending}
+            onClick={onDeleteSeededDataClick}
+            className="cursor-pointer"
+          >
+            Execute
+          </Button>
         </div>
       </CardContent>
     </Card>
