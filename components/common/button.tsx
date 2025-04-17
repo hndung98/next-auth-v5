fb3f2fa20bb19dbb123b325dd7da82dd0b0a55eb
Dung Hoang <hoangdung.200298@gmail.com>
2025-04-17 15:19:19 +0700

    add common components

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -2,6 +2,8 @@
 
 import { logout } from "@/actions/auth";
 import { useRouter } from "next/navigation";
+import { HiOutlinePencil, HiOutlinePlus, HiOutlineTrash } from "react-icons/hi";
+import Link from "next/link";
 
 type LogoutButtonProps = {
   children: React.ReactNode;
@@ -49,3 +51,55 @@ export const MenuItemButton = ({ children, href }: MenuItemButtonProps) => {
     </span>
   );
 };
+
+export function CreateButton({
+  href,
+  buttonText,
+}: {
+  href: string;
+  buttonText: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">{buttonText}</span>{" "}
+      <HiOutlinePlus className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function EditButton({ href }: { href: string }) {
+  return (
+    <Link href={href} className="rounded-md border p-2 hover:bg-gray-100">
+      <HiOutlinePencil className="h-5 w-5" />
+    </Link>
+  );
+}
+
+export function DeleteButton({
+  onAction,
+  id,
+}: {
+  onAction: (id: string) => Promise<{ hasError?: boolean; message?: string }>;
+  id: string;
+}) {
+  return (
+    <button
+      className="rounded-md border p-2 hover:bg-gray-100 cursor-pointer"
+      onClick={async () => {
+        const confirmed = window.confirm("Are you sure?");
+        if (confirmed) {
+          const res = await onAction(id);
+          if (res.hasError) {
+            alert(res.message);
+          }
+        }
+      }}
+    >
+      <span className="sr-only">Delete</span>
+      <HiOutlineTrash className="h-5 w-5" />
+    </button>
+  );
+}
