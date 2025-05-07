23e4ea86a4eeec452207027a369811f04cf445a6
Dung Hoang <hoangdung.200298@gmail.com>
2025-05-07 08:22:23 +0700

    add api - get author by id

---
+++ app/api/authors/[id]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getAuthorById } from "@/data/author";
+import { currentUser } from "@/lib/auth";
+
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const authorId = (await params).id;
+
+    const user = await currentUser();
+    if (!user?.role) return new NextResponse(null, { status: 403 });
+
+    const author = await getAuthorById(authorId);
+
+    if (!author) {
+      return NextResponse.json(
+        { message: "Author not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(author);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(null, { status: 500 });
+  }
+}
