570191381e57d1a944bbd7b68679c23057530a84
Dung Hoang <hoangdung.200298@gmail.com>
2025-05-07 09:02:22 +0700

    update logo

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -1,5 +1,4 @@
-import { MdOutlineElderlyWoman } from "react-icons/md";
-import { GiKatana, GiWhirlwind } from "react-icons/gi";
+import { GiKatana, GiWhirlwind, GiFireExtinguisher } from "react-icons/gi";
 
 import { lusitana } from "@/lib/fonts";
 
@@ -8,8 +7,7 @@ export default function Logo() {
     <div
       className={`${lusitana.className} flex flex-row items-end leading-none text-white`}
     >
-      <MdOutlineElderlyWoman className="h-2/12 w-2/12" />
-      <GiKatana className="h-2/12 w-2/12" />
+      <GiFireExtinguisher className="h-2/12 w-2/12 mx-1" />
       <GiWhirlwind className="h-2/12 w-2/12" />
       <GiWhirlwind className="h-3/12 w-3/12" />
       <GiWhirlwind className="h-5/12 w-5/12" />
