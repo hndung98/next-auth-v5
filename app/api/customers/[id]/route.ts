04ae8675e341149f5c2fbd2543bc483e1a44a85a
Dung Hoang <hoangdung.200298@gmail.com>
2025-05-07 08:21:52 +0700

    organize imports

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { currentUser } from "@/lib/auth";
 import { getCustomerById } from "@/data/customer";
+import { currentUser } from "@/lib/auth";
 
 export async function GET(
   _req: NextRequest,
