833efa12d785b95f4af51f7c46759e18e38ff2a6
Dung Hoang <hoangdung.200298@gmail.com>
2025-05-12 15:39:06 +0700

    update test page

--- app/(public)/test/page.tsx
+++ app/(public)/test/page.tsx
@@ -1,31 +1,26 @@
-import Chat from "./chat";
-
 export default async function Page() {
-  const username = "User" + Math.floor(Math.random() * 1000);
   return (
     <>
       <div className="w-full flex-none md:w-64 justify-center items-center">
         <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
           <div className="hidden h-auto w-full grow rounded-md md:block text-center">
-            {"Online users"}
+            {"Left menu items"}
           </div>
           <div className="hidden h-auto w-full grow rounded-md md:block text-center mt-6">
-            {"User 1"}
+            {"Item 1"}
           </div>
           <div className="hidden h-auto w-full grow rounded-md md:block text-center mt-6">
-            {"User 2"}
+            {"Item 2"}
           </div>
           <div className="hidden h-auto w-full grow rounded-md md:block text-center mt-6">
-            {"User 3"}
+            {"Item 3"}
           </div>
         </div>
       </div>
       <div className="flex-grow p-4 md:overflow-y-auto md:p-6 my-dark-style">
         <div className="p-4 text-center space-y-6">
           <h1>{"(demo)"}</h1>
-          <div>
-            <Chat username={username} />
-          </div>
+          <div className="my-2">---</div>
         </div>
       </div>
     </>
